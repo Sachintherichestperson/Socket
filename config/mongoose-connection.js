497734dfa36f4a636924ef29a97c14616d83ba69
Sachin Bajaj <bajajsachin101@gmail.com>
2025-04-21 05:01:09 +0530

    mongo

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dbgr = require("debug")("production:mongoose");
 const config = require("config");
 
-const MONGO_URI = "MONGODB_URI=mongodb+srv://sachinbajaj:MySecurePass@clartalk.gzh9a.mongodb.net/?retryWrites=true&w=majority"
+const MONGO_URI = "mongodb+srv://sachinbajaj:MySecurePass@clartalk.gzh9a.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.connect(MONGO_URI)
 .then(() => dbgr("MongoDB Connected! 🚀"))
