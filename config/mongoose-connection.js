c56eaf15da9f61dcdc30330127971ea9005f82d4
Sachin Bajaj <bajajsachin101@gmail.com>
2025-04-21 05:01:49 +0530

    mongo

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -4,8 +4,13 @@ const config = require("config");
 
 const MONGO_URI = "mongodb+srv://sachinbajaj:MySecurePass@clartalk.gzh9a.mongodb.net/?retryWrites=true&w=majority"
 
-mongoose.connect(MONGO_URI)
-.then(() => dbgr("MongoDB Connected! 🚀"))
-.catch((err) => dbgr("MongoDB Connection Error:"));
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    connectTimeoutMS: 30000, // Increase the timeout to 30 seconds
+  })
+  .then(() => dbgr("MongoDB Connected! 🚀"))
+  .catch((err) => dbgr("MongoDB Connection Error:", err));
+  
 
 module.exports = mongoose.connection;
